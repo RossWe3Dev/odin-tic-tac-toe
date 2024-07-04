140d2ebd0b94561fe0f746704e75fe9d9c67ea14
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-07-04 23:47:49 +0200

    Separate Game class from main.rb, edit style
    Updated terminal messages color, implemented while loop to keep playing.

---
+++ lib/game.rb
@@ -0,0 +1,66 @@
+class Game
+  WINNING_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]].freeze
+
+  attr_accessor :player_a, :player_b, :board, :current_player
+
+  def initialize
+    @board = Board.new
+    @player_a = Player.new("A", "X".colorize(:red))
+    @player_b = Player.new("B", "O".colorize(:blue))
+    @current_player = nil
+  end
+
+  def play
+    board.display_board
+    game_loop
+    display_winner
+  end
+
+  def turn(player)
+    position = valid_input(player)
+    board.set_marker_position(position - 1, player.marker)
+    board.display_board
+  end
+
+  def valid_input(player)
+    loop do
+      puts "Player #{player.name}, where do you want to put your marker?" \
+           "\n[Please enter a number between 1 and 9]".colorize(:green)
+      position = gets.chomp.to_i
+      return position if (1..9).include?(position) && board.array[position - 1] == " "
+
+      puts "Invalid input. \nPlease enter a number between 1 and 9 that is not already occupied.".colorize(:magenta)
+    end
+  end
+
+  def game_loop
+    @current_player = player_a
+    until board.full?
+      turn(@current_player)
+      break if game_over?(current_player)
+
+      @current_player = switch_current_player
+    end
+  end
+
+  def switch_current_player
+    current_player == player_a ? player_b : player_a
+  end
+
+  def game_over?(player)
+    WINNING_COMBINATIONS.any? do |comb|
+      comb.all? { |position| @board.array[position] == player.marker }
+    end
+  end
+
+  def display_winner
+    puts "Player #{@current_player.name} has won!".colorize(:yellow) if game_over?(current_player)
+    return unless board.full?
+
+    if game_over?(current_player) == true
+      (puts "Player #{@current_player.name} has won!".colorize(:yellow))
+    else
+      (puts "It's a tie!".colorize(:yellow))
+    end
+  end
+end
--- main.rb
+++ main.rb
@@ -2,74 +2,15 @@ require "colorize"
 
 require_relative "lib/board"
 require_relative "lib/player"
+require_relative "lib/game"
 
-class Game
-  WINNING_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]].freeze
-
-  attr_accessor :player_a, :player_b, :board, :current_player
-
-  def initialize
-    @board = Board.new
-    @player_a = Player.new("A", "X".colorize(:red))
-    @player_b = Player.new("B", "O".colorize(:blue))
-    @current_player = nil
-  end
-
-  def play
-    board.display_board
-    game_loop
-    display_winner
-  end
-
-  def turn(player)
-    position = valid_input(player)
-    board.set_marker_position(position - 1, player.marker)
-    board.display_board
-  end
-
-  def valid_input(player)
-    loop do
-      puts "Player #{player.name}, where do you want to put your marker?" \
-           "\n[Please enter a number between 1 and 9]".colorize(:green)
-      position = gets.chomp.to_i
-      return position if (1..9).include?(position) && board.array[position - 1] == " "
-
-      puts "Invalid input. \nPlease enter a number between 1 and 9 that is not already occupied.".colorize(:magenta)
-    end
-  end
-
-  def game_loop
-    @current_player = player_a
-    until board.full?
-      turn(@current_player)
-      break if game_over?(current_player)
-
-      @current_player = switch_current_player
-    end
-  end
-
-  def switch_current_player
-    current_player == player_a ? player_b : player_a
-  end
-
-  def game_over?(player)
-    WINNING_COMBINATIONS.any? do |comb|
-      comb.all? { |position| @board.array[position] == player.marker }
-    end
-  end
-
-  def display_winner
-    puts "Player #{@current_player.name} has won!".colorize(:yellow) if game_over?(current_player)
-    return unless board.full?
-
-    if game_over?(current_player) == true
-      (puts "Player #{@current_player.name} has won!".colorize(:yellow))
-    else
-      (puts "It's a tie!".colorize(:yellow))
-    end
-  end
+def play_game
+  puts "~ Let's play tic-tac-toe! ~".upcase.colorize(:cyan)
+  game = Game.new
+  game.play
+  puts "Press 'y' to play again :) [y/quit]".colorize(:cyan)
+  play_game while gets.chomp.downcase == "y"
+  puts "Thanks for playing!".colorize(:cyan)
 end
 
-puts "Let's play!"
-game1 = Game.new
-game1.play
+play_game
